c3c6d77c9dae46d0a78f4aac9a49538f057848f2
Everton Santos <everton_projeto@outlook.com>
2024-05-17 14:57:41 -0300

    criando supplier de lista de produtos

---
+++ src/p4_interfaces/App.java
@@ -0,0 +1,18 @@
+package p4_interfaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class App {
+    public static void main(String[] args) {
+
+        Supplier<List<Produto>> retornarListaDeProdutos = () -> Arrays.asList(
+                new Produto("Band-Aid", 12.00, 0.25),
+                new Produto("Spray", 107.00, 0.15),
+                new Produto("Teste de gravidez", 55.00, 0.10),
+                new Produto("Esparadrapo", 155.00, 0.25),
+                new Produto("Fralda", 45.00, 0.25)
+        );
+    }
+}
