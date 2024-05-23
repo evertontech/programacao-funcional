3d63f81496e3150f8e5729e6c1922270b32cd317
Everton Santos <everton_projeto@outlook.com>
2024-05-23 14:37:49 -0300

    utilizando todas as funcoes com and then

--- src/p4_interfaces/App.java
+++ src/p4_interfaces/App.java
@@ -9,20 +9,25 @@ public class App {
     public static void main(String[] args) {
 
         Supplier<List<Produto>> retornarListaDeProdutos = () -> Arrays.asList(
-                new Produto("Band-Aid", 12.00, 0.25),
-                new Produto("Spray", 107.00, 0.15),
-                new Produto("Teste de gravidez", 55.00, 0.10),
-                new Produto("Esparadrapo", 155.00, 0.25),
-                new Produto("Fralda", 45.00, 0.25)
+                new Produto("Caneta Esferográfica Premium", 15.75, 0.10),
+                new Produto("Caderno Executivo de Couro", 69.99, 0.15),
+                new Produto("Conjunto de Canetas Técnicas", 40.50, 0.20),
+                new Produto("Lápis de Cor Profissional (36 cores)", 50.25, 0.15),
+                new Produto("Kit de Pincéis Artísticos", 60.80, 0.10),
+                new Produto("Calculadora Científica Avançada", 80.49, 0.10),
+                new Produto("Maleta de Aquarela Profissional", 89.90, 0.10),
+                new Produto("Mochila Térmica para Transporte de Material", 30.20, 0.05),
+                new Produto("Quadro Branco Magnético", 20.15, 0.05),
+                new Produto("Estojo de Canetas Brush Lettering", 10.75, 0.10)
         );
 
-        Function<Produto, Double> calcularPrecoFinal = (produto) -> produto.preco * produto.desconto;
+        Function<Produto, Double> calcularPrecoFinal = (produto) -> produto.preco * (1 - produto.desconto);
 
-        Predicate<Double> temFrete = (preco) -> preco > 50.00;
+        Predicate<Double> temFrete = (preco) -> preco < 50.00;
 
         UnaryOperator<Double> acrescerFrete = (preco) -> {
             if (temFrete.test(preco)) {
-                preco += 9.90;
+                preco += 9.99;
             }
             return preco;
         };
@@ -37,5 +42,15 @@ public class App {
         BiConsumer<Produto, Double> imprimirProduto = (produto, novoPreco) -> {
             System.out.println(produto.nome + ": R$ " + novoPreco);
         };
+
+        retornarListaDeProdutos.get().forEach(produto -> {
+            Double precoFinal = calcularPrecoFinal
+                    .andThen(acrescerFrete)
+                    .andThen(acrescerImposto)
+                    .andThen(arredondarPreco)
+                    .apply(produto);
+
+            imprimirProduto.accept(produto, precoFinal);
+        });
     }
 }
